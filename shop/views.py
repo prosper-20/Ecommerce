79a56441011cd6042b1a411dc2401bf10a7e90a8
prosper <edwardprosper001@gmail.com>
2022-11-03 13:38:44 +0100

    updates

--- shop/views.py
+++ shop/views.py
@@ -6,9 +6,9 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(Category,slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product_list.html',{'category': category,'categories': categories, 'products': products})
+    return render(request, 'shop/product_list.html', {'category': category,'categories': categories, 'products': products})
 
 
 def product_detail(request, id, slug):
