c199e0b074401413f39d780d9732a775d357a243
prosper <edwardprosper001@gmail.com>
2022-11-12 07:16:45 +0100

    updates

--- cart/views.py
+++ cart/views.py
@@ -34,12 +34,9 @@ def cart_detail(request):
                             'override': True})
     return render(request, 'cart/detail.html', {'cart': cart})
 
-from .models import Post
 
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, "blog/home.html", {"posts": posts})
+
 
 
 
