3e8638bad035788f9da0ae41d7c9bf352998691a
prosper <edwardprosper001@gmail.com>
2022-11-21 19:01:28 +0100

    updates

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 
 def order_create(request):
@@ -11,6 +12,8 @@ def order_create(request):
 
         if form.is_valid():
             order = form.save()
+            cart.clear()
+            order_created.delay(order.id)
         
         for item in cart:
             OrderItem.objects.create(order=order,
@@ -21,3 +24,6 @@ def order_create(request):
     else:
         form = OrderCreateForm()
         return render(request,'orders/create.html', {'cart': cart, 'form': form})
+
+
+
