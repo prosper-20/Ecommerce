db60e92cae48811a8400ffb5715bcb4c7d996703
prosper <edwardprosper001@gmail.com>
2022-11-21 19:08:23 +0100

    updates

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.cart import Cart
+from shop.cart import Cart
 from .tasks import order_created
 
 
@@ -9,21 +9,25 @@ def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
-
         if form.is_valid():
             order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            # clear the cart
             cart.clear()
+            # launch asynchronous task
             order_created.delay(order.id)
-        
-        for item in cart:
-            OrderItem.objects.create(order=order,
-            product=item['product'], price=item['price'], quantity=item['quantity'])
-        cart.clear()
-        return render(request, 'orders/created.html', {'order': order})
-    
+            return render(request,
+                          'orders/order/created.html',
+                          {'order': order})
     else:
         form = OrderCreateForm()
-        return render(request,'orders/create.html', {'cart': cart, 'form': form})
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
 
 
 
