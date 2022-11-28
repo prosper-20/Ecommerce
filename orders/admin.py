6fbee1c4bdc10e0aac1a5d9b82b9e098436d8427
prosper <edwardprosper001@gmail.com>
2022-11-28 09:46:29 +0100

    message

--- orders/admin.py
+++ orders/admin.py
@@ -53,7 +53,7 @@ order_pdf.short_description = 'Invoice'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email','address', 'postal_code', 'city', 'paid','created', order_payment, 'updated', order_detail]
+    list_display = ['id', 'first_name', 'last_name', 'email','address', 'postal_code', 'city', 'paid','created', order_payment, 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
